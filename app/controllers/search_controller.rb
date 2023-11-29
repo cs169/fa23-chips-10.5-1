4486c71e5a56369d599789266e0a1e73a94363c0
Ujjwal Krishnamurthi <ujjwal@berkeley.edu>
2023-11-29 22:51:50 +0000

    Error handling + 1.3 updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,9 +7,14 @@ class SearchController < ApplicationController
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
     service.key = Rails.application.credentials.dig(:production, :GOOGLE_API_KEY)
-    result = service.representative_info_by_address(address: address)
-    @representatives = Representative.civic_api_to_representative_params(result)
-    
-    render 'representatives/search'
+    begin
+      result = service.representative_info_by_address(address: address)
+      @representatives = Representative.civic_api_to_representative_params(result)
+      render 'representatives/search'
+    rescue => e 
+      flash[:notice] = "Please input a valid address"
+      @representatives = []
+      render 'representatives/index'
+    end
   end
 end
