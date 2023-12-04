2788e4fb179359ce36567997db3430bd30dea18d
Zwea Htet <zhtet5@berkeley.edu>
2023-12-04 04:48:40 +0000

    complete task 2.1

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'news-api'
 
 class MyNewsItemsController < SessionController
   before_action :set_representative
@@ -6,7 +7,12 @@ class MyNewsItemsController < SessionController
   before_action :set_news_item, only: %i[edit update destroy]
 
   def new
+    @representative = Representative.find(params[:representative_id])
+    @issue = params[:issue]
     @news_item = NewsItem.new
+
+    newsapi = News.new(Rails.application.credentials.dig(:production, :NEWS_API_KEY)) 
+    @articles = newsapi.get_everything(q: @representative.name, sources: 'bbc-news,google-news', language: 'en', country: 'us', sortBy: 'relevancy', pageSize: 5)
   end
 
   def edit; end
@@ -36,6 +42,13 @@ class MyNewsItemsController < SessionController
                 notice: 'News was successfully destroyed.'
   end
 
+  def search_my_news_item
+    @representatives_list = Representative.all.map { |r| [r.name, r.id] }
+    @issues = NewsItem::ISSUES
+
+    redirect_to representative_new_my_news_item_path(@representative)
+  end
+
   private
 
   def set_representative
@@ -56,4 +69,5 @@ class MyNewsItemsController < SessionController
   def news_item_params
     params.require(:news_item).permit(:news, :title, :description, :link, :representative_id)
   end
+
 end
